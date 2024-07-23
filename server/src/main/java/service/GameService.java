49d816ae5f0ee7e9332187c19386f7a133f256f4
AlexGlazier1 <Alexglazier2@gmail.com>
2024-07-22 23:45:46 -0600

    Added more classes

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,8 @@
+package service;
+
+public class GameService {
+    public GameData joinGame(UserData user) {}
+    public GameData createGame(UserData user) {}
+    public GameData listGames(UserData user) {}
+
+}
