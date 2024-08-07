f846afcc8f711c82c2a49bb8addcc61a367af699
AlexGlazier1 <Alexglazier2@gmail.com>
2024-08-06 23:49:45 -0600

    Started layout of phase 4. Created classes for SQL DAOs and moved files from getting started folder

---
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -0,0 +1,27 @@
+package dataaccess;
+
+import model.AuthData;
+
+public class SQLAuthDAO implements AuthDAO {
+    public void createAuth(AuthData auth){
+    }
+
+    public boolean readAuth(String authToken) {
+]
+    }
+
+    public void updateAuth(AuthData auth) throws DataAccessException{
+    }
+
+    public String getUsername(String authToken) throws DataAccessException{
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException{
+    }
+
+    public void deleteAuth(AuthData auth) throws DataAccessException {
+    }
+
+    public void clearAllAuth(){
+    }
+}
---
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -0,0 +1,27 @@
+package dataaccess;
+
+import model.GameData;
+
+import java.util.Map;
+
+public class SQLGameDAO implements GameDAO {
+    public void createGame(GameData Game){
+    }
+
+    public boolean readGame(GameData Game){
+    }
+    public Map<Integer, GameData> listGames(){
+    }
+
+    public void updateGame(GameData Game){
+    }
+
+    public void deleteGame(GameData Game){
+    }
+
+    public GameData getGame(int GameID){
+    }
+
+    public void clearAllGames(){
+    }
+}
